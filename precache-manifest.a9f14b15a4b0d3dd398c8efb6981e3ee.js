7ea1b27b40479db0585d81eff986b21ceac3c112
Sikander Singh <sikander441@gmail.com>
2020-07-25 22:04:35 +0530

    Updates

---
+++ precache-manifest.a9f14b15a4b0d3dd398c8efb6981e3ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9edf7e6e279532f2241540189c52e2",
+    "url": "/algo-viso/index.html"
+  },
+  {
+    "revision": "b1346e9a0e947c2e0b71",
+    "url": "/algo-viso/static/css/main.259f238d.chunk.css"
+  },
+  {
+    "revision": "7acb8d0fe856c00333e9",
+    "url": "/algo-viso/static/js/2.5387f273.chunk.js"
+  },
+  {
+    "revision": "645d538372a1600b69145560985a10a2",
+    "url": "/algo-viso/static/js/2.5387f273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1346e9a0e947c2e0b71",
+    "url": "/algo-viso/static/js/main.72c4d219.chunk.js"
+  },
+  {
+    "revision": "f4a4c0322eff4b6f93cb",
+    "url": "/algo-viso/static/js/runtime-main.96103aba.js"
+  }
+]);
